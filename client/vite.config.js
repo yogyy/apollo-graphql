8409ac98eaee8eb2c21711d5fc6f9503086dcc40
Yogi <yogimr261@gmail.com>
2024-03-02 00:13:58 +0700

    only for github pages

--- client/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import UnoCSS from "unocss/vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), UnoCSS()],
-});

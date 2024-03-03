0ab316b2bc06176148d60d86357406fddf562158
yogyy <yogimr261@gmail.com>
2024-03-03 23:11:39 +0700

    chore: move to ts

--- client/src/components/atom/skeleton.jsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "../../lib/utils";
-// eslint-disable-next-line react/prop-types
-function Skeleton({ className }) {
-  return (
-    <div className={cn("animate-pulse rounded-md bg-primary/10", className)} />
-  );
-}
-
-export { Skeleton };

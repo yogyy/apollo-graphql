edd5a37781828c7a5109af63c31f2a736f2e4ebe
yogyy <yogimr261@gmail.com>
2024-03-04 00:00:45 +0700

    test: deploy vercel

--- server/api/graphql.js
+++ server/api/graphql.js
@@ -2,14 +2,30 @@ import { makeExecutableSchema } from "@graphql-tools/schema";
 import { graphql } from "graphql";
 import { typeDefs } from "../schema/typeDef.js";
 import { resolvers } from "../schema/resolver.js";
+import { createYoga } from "graphql-yoga";
+import cors from "cors";
+import express from "express";
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-/**
- * GraphQL API
- * @param {import("express").Request} req
- * @param {import("express").Response} res
- */
+const yoga = createYoga({
+  schema,
+  graphqlEndpoint: "/graphql",
+});
+// const server = createServer(yoga);
+// server.listen(6000, () => {
+//   console.log("Server is running on http://localhost:6000/graphql");
+// });
+
+const app = express();
+// app.use(cors());
+// app.use(express.json());
+app.use(yoga.graphqlEndpoint, yoga);
+
+app.listen(4000, () => {
+  console.log("☄ Server is running on http://localhost:4000/graphql");
+});
+
 export default async (req, res) => {
   const { query, variables } = JSON.parse(req.body);
   const result = await graphql({

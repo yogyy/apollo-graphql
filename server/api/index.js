52e5bdb24ddeef3412f00ac2070d4690b893a2f0
yogyy <yogimr261@gmail.com>
2024-03-04 23:54:26 +0700

    chore: add cors origin

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,4 @@
 import { makeExecutableSchema } from "@graphql-tools/schema";
-import { graphql } from "graphql";
 import { typeDefs } from "../schema/typeDef.js";
 import { resolvers } from "../schema/resolver.js";
 import { createYoga } from "graphql-yoga";
@@ -12,14 +11,17 @@ const yoga = createYoga({
   schema,
   graphqlEndpoint: "/graphql",
 });
-// const server = createServer(yoga);
-// server.listen(6000, () => {
-//   console.log("Server is running on http://localhost:6000/graphql");
-// });
 
 const app = express();
-app.use(cors());
+
+app.use(
+  cors({ origin: ["http://localhost:5173", "https://yogyy.github.io/"] })
+);
 // app.use(express.json());
 app.use(yoga.graphqlEndpoint, yoga);
+
+// app.listen(3000, () => {
+//   console.log("Server is running on http://localhost:3000/graphql");
+// }); //for local use this
 
 export default app;

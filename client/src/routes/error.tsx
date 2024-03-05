017f707539f6b115d3818bf7bb285918f40bfb12
yogyy <yogimr261@gmail.com>
2024-03-05 14:42:35 +0700

    fix: update error page

--- client/src/routes/error.tsx
+++ client/src/routes/error.tsx
@@ -1,16 +1,37 @@
-import { useRouteError } from "react-router-dom";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
-  console.error(error);
 
-  return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
-  );
+  if (isRouteErrorResponse(error)) {
+    if (error.status === 401) {
+      console.log(error);
+    } else if (error.status === 404) {
+      console.log(error);
+    }
+
+    return (
+      <div id="error-page">
+        <h1 className="text-red-6">Oops! {error.status}</h1>
+        <p>{error.statusText}</p>
+        {error.data?.message && (
+          <p>
+            <i>{error.data.message}</i>
+          </p>
+        )}
+      </div>
+    );
+  } else if (error instanceof Error) {
+    return (
+      <div id="error-page">
+        <h1 className="text-red-6">Oops! Unexpected Error</h1>
+        <p>Something went wrong.</p>
+        <p>
+          <i>{error.message}</i>
+        </p>
+      </div>
+    );
+  } else {
+    return <></>;
+  }
 }
